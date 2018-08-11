836bea56d74ab5851eeb3dd0d75ea1d536bfcd7f
atvanguard <93arpit@gmail.com>
2018-08-11 14:10:12 +0530

    :tada:

---
+++ contracts/InvoiceApp.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.4;
+
+import "@aragon/os/contracts/apps/AragonApp.sol";
+import "@aragon/os/contracts/lib/zeppelin/math/SafeMath.sol";
+
+contract Invoices is AragonApp {
+    using SafeMath for uint256;
+
+    /// Events
+    event Increment(address indexed entity, uint256 step);
+    event Decrement(address indexed entity, uint256 step);
+    event CreatePaymentRequest(string data);
+
+    /// State
+    uint256 public value;
+
+    /// ACL
+    bytes32 constant public INCREMENT_ROLE = keccak256("INCREMENT_ROLE");
+    bytes32 constant public DECREMENT_ROLE = keccak256("DECREMENT_ROLE");
+
+    /**
+     * @notice Increment the counter by `step`
+     * @param step Amount to increment by
+     */
+    function increment(uint256 step) auth(INCREMENT_ROLE) external {
+        value = value.add(step);
+        Increment(msg.sender, step);
+    }
+
+    function CreatePaymentRequest(string data) {
+      emit CreatePaymentRequest(data);
+    }
+
+    /**
+     * @notice Decrement the counter by `step`
+     * @param step Amount to decrement by
+     */
+    function decrement(uint256 step) auth(DECREMENT_ROLE) external {
+        value = value.sub(step);
+        Decrement(msg.sender, step);
+    }
+}
